644ce9924a57c624cbbadabc84cb8f757da6ea97
ferhat.kalkan <ferhat.kalkan@topkapidanismanlik.com.tr>
2023-04-06 18:05:10 +0300

    first commit

---
+++ Prensentation/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Prensentation
+{
+    public static class Prensentation
+    {
+        public static IServiceCollection AddPrensentation(this IServiceCollection services)
+        { 
+            return services;
+        }
+    }
+}
